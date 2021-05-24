cad4bdaeed84773b4a81820095ea9e9307b7829d
Fernando Rocha <fernandorsl@gmail.com>
2021-05-24 14:36:14 -0700

    Delete Pods directory

--- Pods/Target Support Files/Pods-muster-muster-point-client/Pods-muster-muster-point-client-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_muster_muster_point_client : NSObject
-@end
-@implementation PodsDummy_Pods_muster_muster_point_client
-@end
--- Pods/Target Support Files/Pods-muster-muster-point-patrol/Pods-muster-muster-point-patrol-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_muster_muster_point_patrol : NSObject
-@end
-@implementation PodsDummy_Pods_muster_muster_point_patrol
-@end
